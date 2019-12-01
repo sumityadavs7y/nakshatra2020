9903dd8a5686d1940c82a865a82052c59ab90f3c
Sumit Yadav <sumityadavmaster@gmail.com>
2019-12-01 15:17:40 +0530

    env add

--- nakshatra/config.py
+++ nakshatra/config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SECRET_KEY = '8cce3372ecd5b36188ef0172fdba0a00'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost:3306/nakshatra'
+    SECRET_KEY = os.environ['SECRET_KEY']
+    SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']

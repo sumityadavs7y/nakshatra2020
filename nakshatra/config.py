3e6f459c30e58a168650bb63e47b5664e22b6162
Sumit Yadav <sumityadavmaster@gmail.com>
2019-12-02 22:21:30 +0530

    deply ready 3

--- nakshatra/config.py
+++ nakshatra/config.py
@@ -2,4 +2,4 @@ import os
 
 class Config:
     SECRET_KEY =  os.environ['SECRET_KEY']
-    SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
